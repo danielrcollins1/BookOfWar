cdfa4e3ab376e86915d1ad4127b6f08a5955425c
Daniel R. Collins ("Delta") <dcollins@superdan.net>
2022-12-22 22:24:29 -0500

    Clean up styling

--- SpecialAbility.java
+++ SpecialAbility.java
@@ -10,9 +10,9 @@ import java.util.regex.Matcher;
 
 public class SpecialAbility {
 
-	//--------------------------------------------------------------------------
+	//----------------------------------------------------------------------
 	//  Fields
-	//--------------------------------------------------------------------------
+	//----------------------------------------------------------------------
 
 	/** SpecialType of special ability. */
 	private SpecialType type;
@@ -20,76 +20,88 @@ public class SpecialAbility {
 	/** Parameter for level of ability. */
 	private int param;
 
-	//--------------------------------------------------------------------------
+	//----------------------------------------------------------------------
 	//  Constructors
-	//--------------------------------------------------------------------------
+	//----------------------------------------------------------------------
 
 	/**	
 	*  Constructor (type, param).
+	*  @param pType Special type value.
+	*  @param pParam Special parameter value.
 	*/
-	SpecialAbility (SpecialType type, int param) {
-		this.type = type;
-		this.param = param;
+	SpecialAbility(SpecialType pType, int pParam) {
+		type = pType;
+		param = pParam;
 	}
 
 	/**	
 	*  Constructor (type only).
+	*  @param pType Special type value.
 	*/
-	SpecialAbility (SpecialType type) {
-		this(type, 0);
+	SpecialAbility(SpecialType pType) {
+		this(pType, 0);
 	}
 	
 	/**	
 	*  Constructor (copy).
+	*  @param src Special source to copy.
 	*/
-	SpecialAbility (SpecialAbility src) {
+	SpecialAbility(SpecialAbility src) {
 		this(src.type, src.param);
 	}
 	
-	//--------------------------------------------------------------------------
+	//----------------------------------------------------------------------
 	//  Methods
-	//--------------------------------------------------------------------------
+	//----------------------------------------------------------------------
 
 	/**
 	*  Create new special ability from a string.
+	*  @param s Special ability descriptor.
+	*  @return Special ability object.
 	*/
-	static public SpecialAbility createFromString (String s) {
+	public static SpecialAbility createFromString(String s) {
 		Pattern p = Pattern.compile("(\\w+)( \\(([-]?\\d+))?\\)?");
 		Matcher m = p.matcher(s);
 		if (m.matches()) {
 			SpecialType type = SpecialType.findByName(m.group(1));
 			if (type != null) {
-				return m.group(3) == null ?
-					new SpecialAbility(type) :
-					new SpecialAbility(type, Integer.parseInt(m.group(3)));
+				return m.group(3) == null 
+					? new SpecialAbility(type) 
+					: new SpecialAbility(type, 
+						Integer.parseInt(m.group(3)));
 			}
 			else {
-				System.err.println("Error: Unknown special type: " + s);
+				System.err.println(
+					"Error: Unknown special type: " + s);
 				return null;
 			}
 		}
 		else {
-			System.err.println("Error: Invalid special ability format: " + s);
+			System.err.println(
+				"Error: Invalid special ability format: " + s);
 			return null;		
 		}
 	}
 
 	/**
 	*  Get the type of this special ability.
+	*  @return Special type value.
 	*/
-	public SpecialType getType () { 
+	public SpecialType getType() { 
 		return type; 
 	}
 
 	/**
 	*  Get the parameter of this special ability.
+	*  @return Special parameter value.
 	*/
-	public int getParam () { 
+	public int getParam() { 
 		return param; 
 	}
 
 	/**
 	*  Identify this object as a string.
+	*  @return String descriptor.
 	*/
 	public String toString() {
 		String s = type.name();
@@ -101,8 +113,9 @@ public class SpecialAbility {
 	
 	/**
 	*  Main test function.
+	*  @param args Command-line arguments.
 	*/
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		System.out.println(createFromString("MoraleBonus"));
 		System.out.println(createFromString("MoraleBonus (1)"));
 		System.out.println(createFromString("MoraleBonus (2)"));
